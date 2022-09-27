f24e97fd91232a5395dc26cf5515b181c8d32952
AndySch666 <schmidt.hof@web.de>
2022-09-27 15:07:06 +0200

    update styling

--- components/GlobalStyle.js
+++ components/GlobalStyle.js
@@ -1,12 +1,14 @@
-import {createGlobalStyle} from "styled-components";
+import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
 * {
   box-sizing: border-box;
+  color: whitesmoke;
 }
 
 html,
 body {
+  background-color: black;
   padding: 0;
   margin: 0;
   font-family: 'Indie Flower', cursive;
